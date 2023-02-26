f54f8c35021840357775d3864050a291c2093c88
Kelly Schultz <ksjaculus@gmail.com>
2023-02-25 23:03:02 -0500

    Splits pack function into pack_alloc and pack_prealloc

--- src/messagepack_pack.f90
+++ src/messagepack_pack.f90
@@ -1,18 +1,19 @@
 module messagepack_pack
     use iso_fortran_env
     use,intrinsic :: ieee_arithmetic
-    use byte_utilities
     use messagepack_value
 
     implicit none
 
     private
 
-    public :: pack
+    public :: pack_alloc, pack_prealloc
 
     contains
 
-        subroutine pack(mpv, buffer, error)
+        subroutine pack_alloc(mpv, buffer, error)
+            ! Packs a messagepack object into a dynamically allocated buffer,
+            ! returned to the user. The user must handle deallocation of this array
             class(mp_value_type) :: mpv
             logical, intent(out) :: error
             byte, allocatable, dimension(:), intent(out) :: buffer
@@ -20,6 +21,18 @@ module messagepack_pack
 
             call mpv%getsize(dblen) ! get buffer size required
             allocate(buffer(dblen)) ! allocate buffer
+
+            call mpv%pack(buffer, error)
+        end subroutine
+
+        subroutine pack_prealloc(mpv, buffer, error)
+            ! Packs a messagepack object into a pre-allocated buffer,
+            ! returned to the user. This function does not check beforehand
+            ! for the array being the correct size, and will return an error
+            ! if the buffer is too small.
+            class(mp_value_type) :: mpv
+            logical, intent(out) :: error
+            byte, allocatable, dimension(:), intent(inout) :: buffer
 
             call mpv%pack(buffer, error)
         end subroutine
