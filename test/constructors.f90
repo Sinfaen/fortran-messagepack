7f0f57f82ddfeedb4727a62522bf5e9ef1c86565
Kelly Schultz <ksjaculus@gmail.com>
2023-04-03 01:32:33 -0400

    Update map & tests, fixes bugs in arrays and byte utilities
    - additionally fixes bugs in array unpacking
    - adds in get_real for grabbing floating point values
    - fixes bug in byte_utilities
       - turns out 0_real32 molds to int32. fixed by changing to 1.0_real32
    - adds in get_arr_ref & get_map_ref to simplify test code
    
    fixup

--- test/constructors.f90
+++ test/constructors.f90
@@ -8,6 +8,7 @@ program constructors
     type(mp_float_type):: float_test
     type(mp_str_type)  :: str_test
     type(mp_arr_type)  :: arr_test
+    type(mp_map_type)  :: map_test
 
     print *, 'Constructor Test'
 
@@ -24,4 +25,15 @@ program constructors
     arr_test%value(3)%obj = mp_int_type(45)
     arr_test%value(4)%obj = new_real32(-51.4)
     arr_test%value(5)%obj = mp_arr_type(2)
+
+    ! map constructor test
+    map_test = mp_map_type(4)
+    map_test%keys(1)%obj   = mp_int_type(34)
+    map_test%values(1)%obj = mp_str_type("tokay gecko")
+    map_test%keys(2)%obj   = mp_int_type(10)
+    map_test%values(2)%obj = mp_str_type("reticulated python")
+    map_test%keys(3)%obj   = mp_int_type(-2)
+    map_test%values(3)%obj = mp_str_type("caiman")
+    map_test%keys(4)%obj   = mp_int_type(1000)
+    map_test%values(4)%obj = mp_nil_type()
 end program
