e7f53d5e13eb89b26df7a37244b67ef6f2e148ab
Kelly Schultz <ksjaculus@gmail.com>
2023-06-18 16:56:48 -0400

    Add CMake, FPM build. Add simple demo app
    - I'm not quite sure how to handle module linking in a good way for CMake
    - FPM needs a regular executable to build which is partly why I added the simple-demo app
       - this should also serve as a helpful debug tool or a first time user tool

---
+++ app/simple-demo/main.f90
@@ -0,0 +1,44 @@
+! Example demonstrating roundtrip serialization
+! & deserialization.
+program simple_demo
+    use messagepack
+    implicit none
+
+    ! variables
+    class(mp_map_type), allocatable :: mp_map
+    logical :: error
+    class(mp_settings), allocatable :: mp_s
+    byte, dimension(:), allocatable :: buffer
+
+
+    print *, 'Simple MessagePack Demo'
+
+    mp_s = mp_settings()
+
+    ! map assigning id's to types of rodents
+    mp_map = mp_map_type(4_int64) ! pairs
+    mp_map%keys(1)%obj   = mp_str_type("rat")
+    mp_map%values(1)%obj = mp_int_type(5)
+    mp_map%keys(2)%obj   = mp_str_type("gerbil")
+    mp_map%values(2)%obj = mp_int_type(2)
+    mp_map%keys(3)%obj   = mp_str_type("capybara")
+    mp_map%values(3)%obj = mp_int_type(11)
+    mp_map%keys(4)%obj   = mp_str_type("jerboa")
+    mp_map%values(4)%obj = mp_int_type(2)
+
+    ! print the structure to the user
+    print *, "MessagePack map object to be serialized"
+    call print_messagepack(mp_map)
+
+    ! pack the data
+    call pack_alloc(mp_map, buffer, error)
+    if (error) then
+        print *, "[Error: failed to pack mp_map"
+        stop 1
+    end if
+    ! print the buffer
+    print *, "Serialized Data:"
+    print *, buffer
+
+    deallocate(buffer)
+end program
