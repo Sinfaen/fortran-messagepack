59b169b9a0880062f95e8924c3ecba3082c4cc84
Kelly Schultz <ksjaculus@gmail.com>
2023-06-24 23:09:26 -0400

    Add helper functions for unpacking 1d arrays
    - will simplify testing & is also something that is pretty common for users
    - this doesn't handle ndimensional arrays, but messagepack doesn't natively support those either (unfortunately)

--- src/messagepack.f90
+++ src/messagepack.f90
@@ -43,4 +43,70 @@ contains
         write(*,*) "]"
     end subroutine
 
+    subroutine unpack_array_int_1d(obj, om, errored)
+        ! Attempts to unpack a 1d messagepack array of integers
+        ! Note: does not check `is_unsigned`.
+        ! @param[in] obj - messagepack object
+        ! @param[out] om - dynamically allocated matrix
+        ! @param[out] errored - .true. if an error occurred
+        class(mp_value_type), allocatable, intent(in) :: obj
+        integer(kind=int64), dimension(:), allocatable, intent(out) :: om
+        logical, intent(out) :: errored
+
+        ! variables
+        logical :: stat
+        integer(kind=int64) :: i, val, l
+        class(mp_arr_type), allocatable :: arr
+
+        errored = .true.
+        call get_arr_ref(obj, arr, stat)
+        if (.not.(stat)) then
+            return
+        end if
+        ! initialize output
+        l = arr%numelements()
+        allocate(om(l))
+        do i = 1,l
+            call get_int(arr%value(i)%obj, val, stat)
+            if (.not.(stat)) then
+                return
+            end if
+            om(i) = val
+        end do
+        errored = .false.
+    end subroutine
+
+    subroutine unpack_array_real_1d(obj, om, errored)
+        ! Attempts to unpack a 1d messagepack array of reals
+        ! @param[in] obj - messagepack object
+        ! @param[out] om - dynamically allocated matrix
+        ! @param[out] errored - .true. if an error occurred
+        class(mp_value_type), allocatable, intent(in) :: obj
+        real(kind=real64), dimension(:), allocatable, intent(out) :: om
+        logical, intent(out) :: errored
+
+        ! variables
+        logical :: stat
+        integer(kind=int64) :: i, l
+        real(kind=real64) :: val
+        class(mp_arr_type), allocatable :: arr
+
+        errored = .true.
+        call get_arr_ref(obj, arr, stat)
+        if (.not.(stat)) then
+            return
+        end if
+        ! initialize output
+        l = arr%numelements()
+        allocate(om(l))
+        do i = 1,l
+            call get_real(arr%value(i)%obj, val, stat)
+            if (.not.(stat)) then
+                return
+            end if
+            om(i) = val
+        end do
+        errored = .false.
+    end subroutine
+
 end module
